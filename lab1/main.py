170c0b2ff9041ae4c61b17970d71d4bc2fc978be
vaultboy324 <vaultboy324@gmail.com>
2020-03-04 10:05:52 +0300

    finish 1 and start 2

--- lab1/main.py
+++ lab1/main.py
@@ -1,5 +1,5 @@
-from data.data import example
+from data.data import example, my_dataset
 from modules.regression import Regression
 
 if __name__ == '__main__':
-    print(Regression.get_report(example))
+    print(Regression.get_report(my_dataset))
